0d7992cbf90ec2d3e72a21c2755d820f582b99a2
倾盖 <jcmeet@outlook.com>
2023-01-29 16:21:42 +0800

    测试vscode通过remote shh插件远程链接腾讯云服务器进行开发

--- 10-Array数组/1-index.js
+++ 10-Array数组/1-index.js
@@ -1,8 +1,8 @@
 /**
  * 数组对象是使用单独的变量名来存储一系列的值。
  */
-let str=new String('hello')
+var str = new String('hello');
 console.log(str);
-let arr=new Array()
-arr=[1,2,3,4,5]
-console.log(arr)
+var arr = new Array();
+arr = [1, 2, 3, 4, 5];
+console.log(arr);
