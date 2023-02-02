3fb35d804c0c5174b99ba373448f3f2f9e6dc03b
倾盖 <jcmeet@outlook.com>
2023-02-02 19:08:12 +0800

    feat(10-Array数组):新增一些学习Ts Array对象时写的demo

--- 10-Array数组/1-index.js
+++ 10-Array数组/1-index.js
@@ -1,8 +1,52 @@
 /**
  * 数组对象是使用单独的变量名来存储一系列的值。
  */
-var str = new String('hello');
-console.log(str);
-var arr = new Array();
-arr = [1, 2, 3, 4, 5];
-console.log(arr);
+/**
+ * 语法格式
+ */
+var array_name;
+array_name = ['str', 'hello', 'QGai'];
+var array_name = ['world', 'car', 'TypeScript'];
+/**
+ * Array 对象
+ * 我们也可以使用 Array 对象创建数组。
+ * Array 对象的构造函数接受以下两种值：
+ * 表示数组大小的数值。
+ * 初始化的数组列表，元素使用逗号分隔值。
+ */
+var names = new Array(4);
+names = ['Jack', 'Tom', 'Mark', 'QGai'];
+var strArr = new Array('zs', 'ls', 'we', 'mz');
+/**
+ * 数组解构
+ * 我们也可以把数组元素赋值给变量
+ */
+var arr = [12, 13];
+var x = arr[0], y = arr[1];
+console.log(x); // 输出12
+console.log(y); // 输出13
+/**
+ * 可以使用循环来迭代数组
+ */
+var arr = [10086, 10010, 10000, 10086110];
+arr.forEach(function (element) {
+    console.log(element);
+});
+/**
+ * 输出
+    10086
+    10010
+    10000
+    10086110
+ */
+/**
+ * 多维数组
+ * 一个数组的元素可以是另外一个数组，这样就构成了多维数组（Multi-dimensional Array）。
+ * 最简单的多维数组是二维数组，定义方式如下：
+ */
+var multi_arr = [[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]];
+multi_arr.forEach(function (arr) {
+    arr.forEach(function (value) {
+        console.log(value);
+    });
+});
